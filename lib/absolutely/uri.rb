d14e33f2557bb948b976f020017231f9b2fd2dcd
Jason Garber <jason@sixtwothree.org>
2019-05-01 19:43:26 -0400

    Rename to_absolute_uri => to_abs

--- lib/absolutely/uri.rb
+++ lib/absolutely/uri.rb
@@ -1,36 +1,35 @@
 module Absolutely
   class URI
-    attr_reader :base, :relative
+    attr_reader :base, :base_uri, :relative, :relative_uri
 
     def initialize(base:, relative:)
       raise ArgumentError, "base must be a String (given #{base.class.name})" unless base.is_a?(String)
       raise ArgumentError, "relative must be a String (given #{relative.class.name})" unless relative.is_a?(String)
 
+      set_instance_vars(base, relative)
+
+      raise ArgumentError, 'base must be an absolute URL (e.g. https://example.com)' unless base_uri.absolute?
+    end
+
+    def to_abs
+      return relative_uri.to_s if relative_uri.absolute?
+
+      base_uri.path = base_uri.normalized_path
+
+      (base_uri + relative_uri).to_s
+    end
+    alias_method :to_absolute_uri, :to_abs
+
+    private
+
+    def set_instance_vars(base, relative)
       @base = base
       @relative = relative
 
-      base_uri
-      relative_uri
-
-      raise ArgumentError, 'base must be an absolute URL (e.g. https://example.com)' unless base_uri.absolute?
+      @base_uri = Addressable::URI.parse(base)
+      @relative_uri = Addressable::URI.parse(relative)
     rescue Addressable::URI::InvalidURIError => exception
       raise InvalidURIError, exception
-    end
-
-    def base_uri
-      @base_uri ||= Addressable::URI.parse(base)
-    end
-
-    def relative_uri
-      @relative_uri ||= Addressable::URI.parse(relative)
-    end
-
-    def to_absolute_uri
-      return relative_uri.to_s if relative_uri.absolute?
-
-      base_uri.path = '/' if base_uri.path.empty?
-
-      (base_uri + relative_uri).to_s
     end
   end
 end
