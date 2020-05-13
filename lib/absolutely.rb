bb0456e5c54255b1c5303c969662af0c0b28268f
Jason Garber <jason@sixtwothree.org>
2020-05-13 14:35:17 -0400

    Refactor gem code

--- lib/absolutely.rb
+++ lib/absolutely.rb
@@ -7,12 +7,30 @@ require 'absolutely/uri'
 
 module Absolutely
   class << self
+    # Convert a relative path to an absolute URI.
+    #
+    # @param base [String]
+    # @param relative [String]
+    # @return [String]
+    #
+    # @example
+    #   Absolutely.to_abs(base: 'https://example.com', relative: '/foo')
+    #   #=> "https://example.com/foo"
     def to_abs(base:, relative:)
       uri(base: base, relative: relative).to_abs
     end
 
     alias to_absolute_uri to_abs
 
+    # Create an Absolutely::URI from a base URI and a relative path.
+    #
+    # @param base [String]
+    # @param relative [String]
+    # @return [Absolutely::URI]
+    #
+    #   uri = Absolutely.uri(base: 'https://example.com', relative: '/foo')
+    #   puts uri.to_abs
+    #   #=> "https://example.com/foo"
     def uri(base:, relative:)
       URI.new(base: base, relative: relative)
     end
--- lib/absolutely/uri.rb
+++ lib/absolutely/uri.rb
@@ -1,20 +1,51 @@
 module Absolutely
   class URI
-    attr_reader :base, :base_uri, :relative, :relative_uri
+    attr_reader :base, :relative
 
+    # Create an Absolutely::URI from a base URI and a relative path.
+    #
+    # @param base [String]
+    # @param relative [String]
+    # @raise [Absolutely::ArgumentError]
+    # @raise [Absolutely::InvalidURIError]
+    #
+    # @example
+    #   uri = Absolutely::URI.new(base: 'https://example.com', relative: '/foo')
+    #   puts uri.to_abs
+    #   #=> "https://example.com/foo"
     def initialize(base:, relative:)
       raise ArgumentError, "base must be a String (given #{base.class.name})" unless base.is_a?(String)
       raise ArgumentError, "relative must be a String (given #{relative.class.name})" unless relative.is_a?(String)
 
-      set_instance_vars(base, relative)
+      @base = base
+      @relative = relative
+
+      base_uri
+      relative_uri
 
       raise ArgumentError, 'base must be an absolute URL (e.g. https://example.com)' unless base_uri.absolute?
+    rescue Addressable::URI::InvalidURIError => exception
+      raise InvalidURIError, exception
     end
 
+    # @return [Addressable::URI]
+    def base_uri
+      @base_uri ||= Addressable::URI.parse(base)
+    end
+
+    # @return [String]
     def inspect
-      format("#<#{self.class.name}:%#0x>", object_id)
+      format("#<#{self.class.name}:%#0x base: #{base.inspect}, relative: #{relative.inspect}>", object_id)
     end
 
+    # @return [Addressable::URI]
+    def relative_uri
+      @relative_uri ||= Addressable::URI.parse(relative)
+    end
+
+    # Convert a relative path to an absolute URI.
+    #
+    # @return [String]
     def to_abs
       return relative_uri.normalize.to_s if relative_uri.absolute?
 
@@ -24,17 +55,5 @@ module Absolutely
     end
 
     alias to_absolute_uri to_abs
-
-    private
-
-    def set_instance_vars(base, relative)
-      @base = base
-      @relative = relative
-
-      @base_uri = Addressable::URI.parse(base)
-      @relative_uri = Addressable::URI.parse(relative)
-    rescue Addressable::URI::InvalidURIError => exception
-      raise InvalidURIError, exception
-    end
   end
 end
