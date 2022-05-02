668e6cc0bd06a2bb7b44ba31d0b29b438d9d94c4
Jason Garber <jason@sixtwothree.org>
2022-05-02 11:46:14 -0400

    Use e instead of exception

--- lib/absolutely/uri.rb
+++ lib/absolutely/uri.rb
@@ -26,8 +26,8 @@ module Absolutely
       relative_uri
 
       raise ArgumentError, 'base must be an absolute URL (e.g. https://example.com)' unless base_uri.absolute?
-    rescue Addressable::URI::InvalidURIError => exception
-      raise InvalidURIError, exception
+    rescue Addressable::URI::InvalidURIError => e
+      raise InvalidURIError, e
     end
 
     # @return [Addressable::URI]
